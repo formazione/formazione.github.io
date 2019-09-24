0ee8b6ccdabceb1a157578c105c055861029fb15
formazione <gatto.gio@gmail.com>
2019-09-24 18:28:15 +0200

    new stuffs

---
+++ newlinks.js
@@ -0,0 +1,31 @@
+
+// codice javascript che va nel file newlinks js, stessa cartella di index.html
+// dato che il programma si trova una cartella avanti
+// forse devo scrivere ../text/
+newlinks.innerHTML += "<a href='Programmi20192020/text/art2424.html'>art2424.html</a><br>"
+
+                
+// codice javascript che va nel file newlinks js, stessa cartella di index.html
+// dato che il programma si trova una cartella avanti
+// forse devo scrivere ../text/
+newlinks.innerHTML += "<a href='Programmi20192020/text/Bilancio.html'>Bilancio.html</a><br>"
+
+                
+// codice javascript che va nel file newlinks js, stessa cartella di index.html
+// dato che il programma si trova una cartella avanti
+// forse devo scrivere ../text/
+newlinks.innerHTML += "<a href='Programmi20192020/text/Programma 5ce.html'>Programma 5ce.html</a><br>"
+
+                
+// codice javascript che va nel file newlinks js, stessa cartella di index.html
+// dato che il programma si trova una cartella avanti
+// forse devo scrivere ../text/
+newlinks.innerHTML += "<a href='Programmi20192020/text/riparto.html'>riparto.html</a><br>"
+
+                
+// codice javascript che va nel file newlinks js, stessa cartella di index.html
+// dato che il programma si trova una cartella avanti
+// forse devo scrivere ../text/
+newlinks.innerHTML += "<a href='Programmi20192020/text/scorporo1.html'>scorporo1.html</a><br>"
+
+                
