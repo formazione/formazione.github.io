3e53cba5141782a0da36238e5fa519d368aba86a
formazione <gatto.gio@gmail.com>
2019-09-25 07:17:45 +0200

    new stuffs

--- newlinks.js
+++ newlinks.js
@@ -2,30 +2,18 @@
 // codice javascript che va nel file newlinks js, stessa cartella di index.html
 // dato che il programma si trova una cartella avanti
 // forse devo scrivere ../text/
-newlinks.innerHTML += "<a href='Programmi20192020/text/art2424.html'>art2424.html</a><br>"
+newlinks.innerHTML += "<a href='Programmi20192020/text/4ce.html'>4ce.html</a><br>"
 
                 
 // codice javascript che va nel file newlinks js, stessa cartella di index.html
 // dato che il programma si trova una cartella avanti
 // forse devo scrivere ../text/
-newlinks.innerHTML += "<a href='Programmi20192020/text/Bilancio.html'>Bilancio.html</a><br>"
+newlinks.innerHTML += "<a href='Programmi20192020/text/5bs_programma.html'>5bs_programma.html</a><br>"
 
                 
 // codice javascript che va nel file newlinks js, stessa cartella di index.html
 // dato che il programma si trova una cartella avanti
 // forse devo scrivere ../text/
-newlinks.innerHTML += "<a href='Programmi20192020/text/Programma 5ce.html'>Programma 5ce.html</a><br>"
-
-                
-// codice javascript che va nel file newlinks js, stessa cartella di index.html
-// dato che il programma si trova una cartella avanti
-// forse devo scrivere ../text/
-newlinks.innerHTML += "<a href='Programmi20192020/text/riparto.html'>riparto.html</a><br>"
-
-                
-// codice javascript che va nel file newlinks js, stessa cartella di index.html
-// dato che il programma si trova una cartella avanti
-// forse devo scrivere ../text/
-newlinks.innerHTML += "<a href='Programmi20192020/text/scorporo1.html'>scorporo1.html</a><br>"
+newlinks.innerHTML += "<a href='Programmi20192020/text/links.html'>links.html</a><br>"
 
                 
