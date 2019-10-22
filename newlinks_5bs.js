9e993a39766b25167134ddffce46d03d6fbb0d72
formazione <gatto.gio@gmail.com>
2019-10-22 06:54:54 +0200

    new stuffs

--- newlinks_5bs.js
+++ newlinks_5bs.js
@@ -1,9 +1,8 @@
 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/000_Programma.html'>000_Programma.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/001_Marketing_definizione.html'>001_Marketing_definizione.html</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/002_Marketing.html'>002_Marketing.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Esercizio_Turismo.html'>Esercizio_Turismo.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/punteggi.html'>punteggi.html</a><br>"
-                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Regolamenti e direttive.html'>Regolamenti e direttive.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Turismo responsabile.html'>Turismo responsabile.html</a><br>"
-                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Turismo_e_sviluppo.html'>Turismo_e_sviluppo.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Turismo_SINTESI.html'>Turismo_SINTESI.html</a><br>"
                 
