a947c3b39cc1a56d7234cbfdfe72bf5a7eded6bb
formazione <gatto.gio@gmail.com>
2019-10-26 07:28:05 +0200

    new stuffs

--- newlinks_5ce.js
+++ newlinks_5ce.js
@@ -1,7 +1,7 @@
 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Bilancio_esercizi.html'>Bilancio_esercizi.html</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Esercizi_cambio.html'>Esercizi_cambio.html</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Punteggi.html'>Punteggi.html</a><br>"
-                newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/turismo.html'>turismo.html</a><br>"
-                newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Turismo_e_sviluppo.html'>Turismo_e_sviluppo.html</a><br>"
+                newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/turismo_5bs.html'>turismo_5bs.html</a><br>"
+                newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Turismo_5ce.html'>Turismo_5ce.html</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Turismo_responsabile.html'>Turismo_responsabile.html</a><br>"
                 
