71d7cd02a5508a7a6aba1706087a287a3735ea60
formazione <gatto.gio@gmail.com>
2019-10-29 14:08:57 +0100

    new stuffs

--- newlinks_5bs.js
+++ newlinks_5bs.js
@@ -2,6 +2,7 @@ newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/000_Programma.htm
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/001_Marketing_definizione.html'>001_Marketing_definizione.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/002_Marketing.html'>002_Marketing.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Esercizio_Turismo.html'>Esercizio_Turismo.html</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/mission.html'>mission.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/punteggi.html'>punteggi.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Turismo responsabile.html'>Turismo responsabile.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Turismo_SINTESI.html'>Turismo_SINTESI.html</a><br>"
