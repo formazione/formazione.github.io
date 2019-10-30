6ab025767d22b2c47bcec893604f2a1c5d646f3b
formazione <gatto.gio@gmail.com>
2019-10-30 05:50:06 +0100

    new stuffs

--- newlinks_5bs.js
+++ newlinks_5bs.js
@@ -1,6 +1,7 @@
 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/000_Programma.html'>000_Programma.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/001_Marketing_definizione.html'>001_Marketing_definizione.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/002_Marketing.html'>002_Marketing.html</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/30102019.html'>30102019.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Esercizio_Turismo.html'>Esercizio_Turismo.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/mission.html'>mission.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/punteggi.html'>punteggi.html</a><br>"
