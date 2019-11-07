ee47dc02eea3bfbc30a973af7d60dcac2729e7e8
formazione <gatto.gio@gmail.com>
2019-11-07 18:39:01 +0100

    new stuffs

--- newlinks_5bs.js
+++ newlinks_5bs.js
@@ -3,6 +3,6 @@ newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/5bs_es1.html'>5bs
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Esercizio_30102019.html'>Esercizio_30102019.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Marketing.html'>Marketing.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Programma.html'>Programma.html</a><br>"
-                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Statistiche_classe.html'>Statistiche_classe.html</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/punteggi.html'>punteggi.html</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/todo.html'>todo.html</a><br>"
                 
