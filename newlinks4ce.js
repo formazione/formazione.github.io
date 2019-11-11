e1f20150939c83507f51b7c2a6503939e331154d
Giovanni Gatto <37396735+formazione@users.noreply.github.com>
2019-11-11 19:24:25 +0100

    Update newlinks4ce.js

--- newlinks4ce.js
+++ newlinks4ce.js
@@ -1,7 +1,2 @@
-newlinks4ce.innerHTML += "<a href='Programmi20192020/text_5ce/000_programma.html'>000_programma.html</a><br>"
-                newlinks4ce.innerHTML += "<a href='Programmi20192020/text_5ce/Partita doppia.html'>Partita doppia.html</a><br>"
-                newlinks4ce.innerHTML += "<a href='Programmi20192020/text_5ce/Punt.html'>Punt.html</a><br>"
-                newlinks4ce.innerHTML += "<a href='Programmi20192020/text_5ce/Punteggi.html'>Punteggi.html</a><br>"
-                newlinks4ce.innerHTML += "<a href='Programmi20192020/text_5ce/turismo.html'>turismo.html</a><br>"
-                newlinks4ce.innerHTML += "<a href='Programmi20192020/text_5ce/Turismo_responsabile.html'>Turismo_responsabile.html</a><br>"
+newlinks4ce.innerHTML += "<a href='Programmi20192020/text_5ce/turismo.html'>turismo.html</a><br>"
                 
