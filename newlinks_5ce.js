eee9d355b0a8963706f2a497d8d39576035939ea
formazione <gatto.gio@gmail.com>
2019-11-22 09:18:23 +0100

    new stuffs

--- newlinks_5ce.js
+++ newlinks_5ce.js
@@ -2,4 +2,5 @@ newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Bilancio.html'>Bi
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Cambio.html'>Cambio</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Punteggi.html'>Punteggi</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Turismo.html'>Turismo</a><br>"
+                newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Turismo2.html'>Turismo2</a><br>"
                 
