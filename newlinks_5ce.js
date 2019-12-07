8039507b11b74b728071540d7da20c390eb9ff92
formazione <gatto.gio@gmail.com>
2019-12-07 08:12:15 +0100

    new stuffs

--- newlinks_5ce.js
+++ newlinks_5ce.js
@@ -3,6 +3,7 @@ newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Bilancio.html'>Bi
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Punteggi.html'>Punteggi</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Test_turismo_1.html'>Test_turismo_1</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Test_turismo_2.html'>Test_turismo_2</a><br>"
+                newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Test_turismo_3.html'>Test_turismo_3</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Turismo.html'>Turismo</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Turismo2.html'>Turismo2</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Turismo_appunti_completo.html'>Turismo_appunti_completo</a><br>"
