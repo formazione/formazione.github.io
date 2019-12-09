c2e8b81a858580655389aec091e55b8d12514f6a
formazione <gatto.gio@gmail.com>
2019-12-09 07:48:32 +0100

    new stuffs

--- newlinks_5bs.js
+++ newlinks_5bs.js
@@ -1,4 +1,5 @@
-newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Marketing.html'>Marketing</a><br>"
+newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Ciclo di vita.html'>Ciclo di vita</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Marketing.html'>Marketing</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/punteggi.html'>punteggi</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/SWOT.html'>SWOT</a><br>"
                 
