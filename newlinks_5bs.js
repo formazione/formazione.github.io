b8680e0024c06f5d0825ff539f87329456fe8e5d
formazione <gatto.gio@gmail.com>
2019-12-12 18:21:11 +0100

    new stuffs

--- newlinks_5bs.js
+++ newlinks_5bs.js
@@ -1,6 +1,6 @@
-newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/1929.html'>1929.html</a><br>"
-                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Ciclo di vita.html'>Ciclo di vita.html</a><br>"
-                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Marketing.html'>Marketing.html</a><br>"
-                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/punteggi.html'>punteggi.html</a><br>"
-                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/SWOT.html'>SWOT.html</a><br>"
+newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/1929.html'>1929</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Ciclo di vita.html'>Ciclo di vita</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Marketing.html'>Marketing</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/punteggi.html'>punteggi</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/SWOT.html'>SWOT</a><br>"
                 
