ddf8aea4bf8a9b92d7651c4e3e0a9814425f8d47
formazione <gatto.gio@gmail.com>
2020-01-28 05:53:02 +0100

    new stuffs

--- newlinks_4ce.js
+++ newlinks_4ce.js
@@ -1,8 +1,5 @@
 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/10_DOMANDE_BILANCIO.html'>10_DOMANDE_BILANCIO</a><br>"
-                newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/4ce_test2.html'>4ce_test2</a><br>"
-                newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/Conti_Risposta_Multipla.html'>Conti_Risposta_Multipla</a><br>"
-                newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/Es_conti_2020_1.html'>Es_conti_2020_1</a><br>"
-                newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/ES_giornale1.html'>ES_giornale1</a><br>"
+                newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/ESERCIZI.html'>ESERCIZI</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/Finanziamenti.html'>Finanziamenti</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/punteggi.html'>punteggi</a><br>"
                 
