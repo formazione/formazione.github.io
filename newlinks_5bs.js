2ced7b8c6a50ea10369ebb88b7658a2d999b71ba
formazione <gatto.gio@gmail.com>
2020-02-04 06:45:36 +0100

    new stuffs

--- newlinks_5bs.js
+++ newlinks_5bs.js
@@ -1,3 +1,4 @@
-newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Marketing operativo SINTESI.html'>Marketing operativo SINTESI</a><br>"
+newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Business_plan.html'>Business_plan</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Marketing operativo SINTESI.html'>Marketing operativo SINTESI</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/punteggi.html'>punteggi</a><br>"
                 
