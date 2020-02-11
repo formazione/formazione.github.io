0581a17931d818e474827732fdba379bd23f9f74
formazione <gatto.gio@gmail.com>
2020-02-11 06:07:51 +0100

    new stuffs

--- newlinks_5ce.js
+++ newlinks_5ce.js
@@ -2,5 +2,6 @@ newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/budget_test1.html
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/modulo_0_bilancio.html'>modulo_0_bilancio</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/modulo_1_turismo.html'>modulo_1_turismo</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/modulo_2_budget.html'>modulo_2_budget</a><br>"
+                newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/modulo_3_Business_plan.html'>modulo_3_Business_plan</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Punteggi.html'>Punteggi</a><br>"
                 
