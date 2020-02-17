3393c6a175f246d39a195d16e17cba1345369e07
formazione <gatto.gio@gmail.com>
2020-02-17 20:18:40 +0100

    another update

--- Programmi20192020/commit.py
+++ Programmi20192020/commit.py
@@ -3,9 +3,11 @@ import os
 def commit():
     os.chdir("..")
     print(os.getcwd())
-    os.system("git pull")
+    #os.system("git pull")
     os.system("git add .")
     os.system("git commit -m \"another update\"")
     os.system("git push")
     #os.startfile("..\\commit.bat")
     print("All done")
+
+commit()
---
+++ Programmi20192020/commit_bat.py
@@ -0,0 +1,6 @@
+# start bat
+
+import os
+
+os.system("start commit.bat")
+print("I committed")
