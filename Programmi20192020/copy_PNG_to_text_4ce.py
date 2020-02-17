27d7af9d29e8249549ae991a635f4ff055055356
formazione <gatto.gio@gmail.com>
2020-02-17 20:24:49 +0100

    new stuffs

---
+++ Programmi20192020/copy_PNG_to_text_4ce.py
@@ -0,0 +1,8 @@
+# copy file
+
+import shutil
+
+
+filedacopiare = "Donnianni.PNG"
+shutil.copyfile("H:\\avatars\\" + filedacopiare, "H:\\formazione.github.io\\formazione.github.io\\Programmi20192020\\text_4ce\\" + filedacopiare)
+
