7efc2e648200c681fafd2c154509c21692485488
formazione <gatto.gio@gmail.com>
2020-02-25 18:40:06 +0100

    new stuffs

--- Programmi20192020/copy_PNG_to_text_4ce.py
+++ Programmi20192020/copy_PNG_to_text_4ce.py
@@ -3,9 +3,10 @@
 import shutil
 from glob import glob
 
-filedacopiare = glob("*.PNG")
-src = "H:\\avatars\\5bs\\"
+f = "DApolito Giuliano.PNG"
+src = "H:\\avatars\\"
 dest = "H:\\formazione.github.io\\formazione.github.io\\Programmi20192020\\text_5bs\\"
-for f in filedacopiare:
-    shutil.copyfile(src + f, dest + f)
+# for f in filedacopiare:
+
+shutil.copyfile(src + f, dest + f)
 
