2a20cc1d3a16baac5479da963da1a9a7f8741f71
formazione <gatto.gio@gmail.com>
2020-02-28 14:09:59 +0100

    new stuffs

--- Programmi20192020/copy_PNG_to_text_4ce.py
+++ Programmi20192020/copy_PNG_to_text_4ce.py
@@ -2,10 +2,11 @@
 
 import shutil
 from glob import glob
-
-f = "DApolito Giuliano.PNG"
+f = input("Name of the file in avatars to copy")
+c = input("Name of the class (4ce, 5ce, 5bs")
+# f = "DApolito Giuliano.PNG"
 src = "H:\\avatars\\"
-dest = "H:\\formazione.github.io\\formazione.github.io\\Programmi20192020\\text_5bs\\"
+dest = f"H:\\formazione.github.io\\formazione.github.io\\Programmi20192020\\text_{c}\\"
 # for f in filedacopiare:
 
 shutil.copyfile(src + f, dest + f)
