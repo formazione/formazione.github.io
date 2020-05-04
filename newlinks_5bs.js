5b7b758999eb16ee8c4fc5e08bf30bb3688ba6c8
formazione <gatto.gio@gmail.com>
2020-05-04 09:42:06 +0200

    new stuffs

--- newlinks_5bs.js
+++ newlinks_5bs.js
@@ -1,5 +1,6 @@
 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/0_TEST.html'>0_TEST</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/2_MARKETING.html'>2_MARKETING</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/3_BUSINESS PLAN.html'>3_BUSINESS PLAN</a><br>"
+                newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/4_CONTRATTO.html'>4_CONTRATTO</a><br>"
                 newlinks_5bs.innerHTML += "<a href='Programmi20192020/text_5bs/Didattica a distanza.html'>Didattica a distanza</a><br>"
                 
