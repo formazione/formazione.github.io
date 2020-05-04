a8aebbcf39186ba178b01b93db240a940073d432
formazione <gatto.gio@gmail.com>
2020-05-04 09:26:33 +0200

    new stuffs

--- newlinks_4ce.js
+++ newlinks_4ce.js
@@ -2,6 +2,7 @@ newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/0_TEST.html'>0_TE
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/1_FINANZIAMENTI.html'>1_FINANZIAMENTI</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/2_BILANCIO.html'>2_BILANCIO</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/3_INDICI.html'>3_INDICI</a><br>"
+                newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/contratto.html'>contratto</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/Didattica a distanza.html'>Didattica a distanza</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/Test 6.html'>Test 6</a><br>"
                 
