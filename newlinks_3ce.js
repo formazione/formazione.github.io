0ecaefad2d79574ac0877dc6fb101dd699fba18d
formazione <gatto.gio@gmail.com>
2020-10-05 06:20:46 +0200

    new stuffs

--- newlinks_3ce.js
+++ newlinks_3ce.js
@@ -1,2 +1,2 @@
-newlinks_3ce.innerHTML += "<a href='Programmi20192020/3ce/01_percentuali.html'>01_percentuali</a><br>"
+newlinks_3ce.innerHTML += "<a href='Programmi20192020/3ce/Modulo_1_percentuali.html'>Modulo_1_percentuali</a><br>"
                 
