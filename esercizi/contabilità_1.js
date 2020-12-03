bec12968dcccb1e532d64358fd9c85b5927d922f
formazione <gatto.gio@gmail.com>
2020-12-03 16:35:33 +0100

    new stuffs

---
+++ esercizi/contabilità_1.js
@@ -0,0 +1,25 @@
+quiz = [{
+	        "question"      :   "Le scritture obbligatorie",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ["variano in base alla forma giuridica dell'impresa", 'sono uguali per tutte le imprese'],
+	        "correct"       :   "variano in base alla forma giuridica dell'impresa",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "È una scrittura obbligatoria",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['Il libro giornale', 'Il libro Mastro', 'La prima nota'],
+	        "correct"       :   "Il libro giornale",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "I piccoli commercianti devono tenere le scritture obbligatorie previste dal codice civile?",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['No', 'Sì'],
+	        "correct"       :   "No",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "secondo la tesi prevalente sono piccoli imprenditori coloro che esercitano un'attività professionale organizzata prevalentemente con il lavoro proprio e dei componenti della famiglia e con prevalenza di detto lavoro sul capitale proprio od altrui investito nell'impresa (da ciò discende che non potrà essere considerato piccolo imprenditore chi impiega ingenti capitali, anche se lavora da solo, ad es. un gioielliere) e sul lavoro altrui ed in particolare i coltivatori diretti del fondo, gli artigiani, i piccoli commercianti",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['art. 2083', 'art. 2082'],
+	        "correct"       :   "art. 2083",
+	        "explanation"   :   "",
+	    },];
