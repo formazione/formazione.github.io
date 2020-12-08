4ff65a7144c76e665719dfa85ed92bf1972ba2f6
formazione <gatto.gio@gmail.com>
2020-12-08 10:53:21 +0100

    new stuffs

--- newlinks_5ce.js
+++ newlinks_5ce.js
@@ -9,4 +9,5 @@ newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/00_Modulo1_Es_1.h
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/6_CONTRATTO.html'>6_CONTRATTO</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/7_CONTRATTO_RISTORAZIONE.html'>7_CONTRATTO_RISTORAZIONE</a><br>"
                 newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/Didattica a distanza.html'>Didattica a distanza</a><br>"
+                newlinks_5ce.innerHTML += "<a href='Programmi20192020/text_5ce/00_Modulo1_Verifica2.html'>00_Modulo1_Verifica2</a><br>"
                 
