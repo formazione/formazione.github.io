8375f7207d45821f0d7eb402e19a31d1f30caf2b
formazione <gatto.gio@gmail.com>
2021-05-28 20:30:06 +0200

    new commit

---
+++ esercizi_DARM_all/output/lavoro.js
@@ -0,0 +1,49 @@
+quiz = [{
+	        "question"      :   "La domanda di lavoro è formata",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['dalle richieste delle aziende di persona', 'da tutte le persone in cerca di occupazione'],
+	        "correct"       :   "dalle richieste delle aziende di persona",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "A tecnologie più avanzate corrisponde una necessità di manodopera",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['minore', 'maggiore'],
+	        "correct"       :   "minore",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "L'IRPEF è un'imposta",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['progressiva', 'proporzionale', 'logaritmica', 'esponenziale'],
+	        "correct"       :   "progressiva",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "Qual è l'aliquota del secondo scaglione di reddito dell'IRPEF?",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['27', '23', '25', '24'],
+	        "correct"       :   "27",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "L'aliquota per chi ha un reddito di 1.250 € è di",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['287,50', '300,00', '100,00', '250,00'],
+	        "correct"       :   "287,50",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "I contributi sociali INPS vengono",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['sottratti dal reddito lordo', 'aggiunti al reddito lordo'],
+	        "correct"       :   "sottratti dal reddito lordo",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "Il credito dell'art.13 del TUIR al massimo è",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['80 €', '60 €', '40 €', '50 €'],
+	        "correct"       :   "80 €",
+	        "explanation"   :   "",
+	    },{
+	        "question"      :   "Prevede una riduzione di orario il contratto",
+	        "image"         :   "https://tuocoach.files.wordpress.com/2018/11/cambiamento-10.jpg?w=345&h=294",
+	        "choices"       :   ['part time', 'a tempo indeterminato', 'a tempo determinato'],
+	        "correct"       :   "part time",
+	        "explanation"   :   "",
+	    },];
