983b31f7f3b090db04fa92944c74c2e50e79ca52
formazione <gatto.gio@gmail.com>
2021-10-14 15:30:58 +0200

    new stuffs

--- newlinks_4ce.js
+++ newlinks_4ce.js
@@ -1,5 +1,6 @@
 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/0_TEST.html'>0_TEST</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/1_FINANZIAMENTI.html'>1_FINANZIAMENTI</a><br>"
+                newlinks_4ce.innerHTML += "<a href='finanziamenti\\fabbisogno1.html'>Es. fabbbisogno finanziario 1</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/2_BILANCIO.html'>2_BILANCIO</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/3_INDICI.html'>3_INDICI</a><br>"
                 newlinks_4ce.innerHTML += "<a href='Programmi20192020/text_4ce/4_contratto.html'>4_contratto</a><br>"
--- newlinks_4ce_2020.js
+++ newlinks_4ce_2020.js
@@ -1,6 +1,7 @@
 newlinks_4ce_2020.innerHTML += "<a href='Programmi20192020/4ce/01_Modulo_1_es1.html'>01_Modulo_1_es1</a><br>"
                 newlinks_4ce_2020.innerHTML += "<a href='Programmi20192020/4ce/0_TEST.html'>0_TEST</a><br>"
                 newlinks_4ce_2020.innerHTML += "<a href='Programmi20192020/4ce/1_FINANZIAMENTI.html'>1_FINANZIAMENTI</a><br>"
+                newlinks_4ce_2020.innerHTML += "<a href='finanziamenti\\fabbisogno1.html'>Es. fabbbisogno finanziario 1</a><br>"
                 newlinks_4ce_2020.innerHTML += "<a href='Programmi20192020/4ce/2_BILANCIO.html'>2_BILANCIO</a><br>"
                 newlinks_4ce_2020.innerHTML += "<a href='Programmi20192020/4ce/3_INDICI.html'>3_INDICI</a><br>"
                 newlinks_4ce_2020.innerHTML += "<a href='Programmi20192020/4ce/4_contratto.html'>4_contratto</a><br>"
