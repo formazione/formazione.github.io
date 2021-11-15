6ca63b460fa14401c093e243e96204430decd6c7
formazione <gatto.gio@gmail.com>
2021-11-15 20:25:30 +0100

    new commit

---
+++ quiz4ce/crea_link_alunni.py
@@ -0,0 +1,19 @@
+classe = [
+        "balbi",
+        "bertolini",
+        "cuda",
+        "divivo",
+        "iuliano",
+        "lapastina",
+        "lakrad",
+        "maffia",
+        "mercanti",
+        "nese",
+        "niglio",
+        "pica",
+        "spinelli"
+]
+
+for n in classe:
+       print(f"<h2><a href='https://formazione.github.io/quiz5ce/{n}.html'>{n}</a></h2>")
+
