c3ec96c099fb696736333511288b0d5de0be7ebd
formazione <gatto.gio@gmail.com>
2021-11-24 09:46:16 +0100

    new commit

---
+++ quiz5ae/crea_link_alunni.py
@@ -0,0 +1,25 @@
+classe = [
+        "Breglia",
+        "Cammarano_D",
+        "Cammarano_N",
+        "Costantini",
+        "Del Verme,",
+        "Esposito",
+        "Fontana",
+        "Garofalo",
+        "Giordano",
+        "Grimaldi",
+        "Lista",
+        "Malzone",
+        "Mandia",
+        "Marchesani",
+        "Musto",
+        "Ruocco",
+        "Santoro",
+        "Scola_M",
+        "SCOLA_V",
+]
+
+for n in classe:
+       print(f"<h2><a href='https://formazione.github.io/quiz5ae/{n}.html'>{n}</a></h2>")
+
